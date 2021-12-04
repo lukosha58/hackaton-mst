10ce0f57e0201c4dc74ff14401e7fcb1502e0f73
Vo5torg <ilya.mayorov.2003@list.ru>
2021-12-05 01:27:37 +0300

    Mew

--- help_func.py
+++ help_func.py
@@ -5,14 +5,44 @@ from telebot import types
 token = open("token").readline()
 bot = telebot.TeleBot(token)
 
-INLINE_MENU = [[["Курсы", "courses"], ["Тесты", "test"]], ["Рейтинг", "rating"]]
-INLINE_VIEW_THEME = [[["Следующая тема", "theme_"], ["Предыдущая тема", "theme_"]], ["Меню", "menu"]]
-INLINE_THEMES = [["Пароли", "theme_0"], ["Транспорт", "theme_1"], ["Qr-код", "theme_2"], ]
+INLINE_MENU = [[["Курс", "courses"], ["Тесты", "test"]], ["Рейтинг", "rating"]]
+INLINE_VIEW_THEME = [[["Следующая тема", "theme_"], ["Предыдущая тема", "theme_"]], ["Завершить курс", "final_courses"],
+                     ["Меню", "menu"]]
+INLINE_THEMES = [["Пароли", "theme_0"], ["Транспорт", "theme_1"], ["Qr-код", "theme_2"], ["4", "theme_3"],
+                 ["5", "theme_4"], ["6", "theme_5"], ["7", "theme_6"], ["8", "theme_7"], ["Меню", "menu"]]
 INLINE_YES_NO = [[["Да", "yes"], ["Нет", "no"]]]
-BUTTON_MENU = ["Меню", ["пися", "попа"]]
+BUTTON_MENU = ["Меню"]
 COURSES = {"0": "https://telegra.ph/Password-12-04-2", "1": "https://telegra.ph/Transport-12-04-2",
-           "2": "https://telegra.ph/QR-12-04"}
-users_info = []
+           "2": "https://telegra.ph/QR-12-04", "3": "3", "4": "4", "5": "5", "6": "6", "7": "7"}
+texts_tree = {"hello": "Здравствуйте, {}",
+              "reg_fio": "Введите своё ФИО",
+              "help": "Помогаю",
+              "wtf": "Не понимаю что ты пишешь",
+              "menu": "Меню",
+              "done_course": "Вау ты прошёл весь курс",
+              "not_done_course": "Нет, ещё не все курсы сделаны",
+              "choose_themes": "Выберите тему",
+              "are_you_ready": "Вы уверены, что готовы пройти тест?",
+              "rating": "А тут рейтинг"}
+
+
+def is_done_full_course(db, message):
+    info = db.get_course_step(message.from_user.id)
+    for i in range(1, len(info)):
+        if info[i] in (False, None):
+            return False
+    return True
+
+
+def get_bool_theme(inline_items, db, message):
+    inline_items = copy.deepcopy(inline_items)
+    users_done_theme = db.get_course_step(message.from_user.id)
+    for i in range(len(inline_items) - 1):
+        if users_done_theme[i + 1]:
+            inline_items[i][0] += " ✅"
+        else:
+            inline_items[i][0] += " ❌"
+    return inline_items
 
 
 def get_keyboard_button(button_items, resize_keyboard=True, one_time_keyboard=True):
@@ -42,31 +72,32 @@ def get_inline_button(inline_items, row_width=3):
 
 
 def check_theme_num(data):
-    for i in range(0, 4):
+    for i in range(len(INLINE_THEMES) - 1):
         if str(i) in data:
             return str(i)
 
 
-def edit_inline_button(course_num, inline_view_course):
+def edit_inline_button(theme_num, inline_view_course, db, message):
     list_courses = copy.deepcopy(inline_view_course)
-    if course_num == str(0):
+
+    if theme_num == str(0):
         del list_courses[0][1]
-        list_courses[0][0][1] += str(int(course_num) + 1)
+        list_courses[0][0][1] += str(int(theme_num) + 1)
         return list_courses
-    elif course_num == str(len(inline_view_course)):
-        del list_courses[0][0]
-        list_courses[0][0][1] += str(int(course_num) - 1)
+    elif theme_num == str(len(INLINE_THEMES) - 2):
+        list_courses[0][1][1] += str(int(theme_num) - 1)
         return list_courses
     else:
-        list_courses[0][0][1] += str(int(course_num) + 1)
-        list_courses[0][1][1] += str(int(course_num) - 1)
-
+        list_courses[0][0][1] += str(int(theme_num) + 1)
+        list_courses[0][1][1] += str(int(theme_num) - 1)
+    if not is_done_full_course(db, message):
+        del list_courses[1]
     return list_courses
 
 
 def delete_last_messages(message):
-    bot.delete_message(message.chat.id, message.message_id)
     try:
+        bot.delete_message(message.chat.id, message.message_id)
         bot.delete_message(message.chat.id, message.message_id - 1)
     except:
         pass
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import database as db
 from help_func import *
+import emoji
 
 db.create_users_table()
 db.create_test_result_table()
@@ -12,51 +13,53 @@ def send_welcome(message):
     def get_username(message):  # получаем ФИО
         inline_buttons = get_inline_button(INLINE_MENU, 2)
         db.insert_user(message.from_user.id, message.from_user.username, message.text)
-        bot.send_message(message.from_user.id, text="Здраствуйте", reply_markup=inline_buttons)
+        bot.send_message(message.from_user.id, text=texts_tree['hello'].format(message.from_user.username),
+                         reply_markup=inline_buttons)
         delete_last_messages(message)
 
     if not db.get_user(message.from_user.id):
-        bot.send_message(message.from_user.id, text="Пожалуйста введите ФИО")
+        bot.send_message(message.from_user.id, text=texts_tree['reg_fio'])
         bot.register_next_step_handler(message, get_username)
     else:
         inline_buttons = get_inline_button(INLINE_MENU, 2)
-        bot.send_message(message.from_user.id, text="Здраствуйте", reply_markup=inline_buttons)
+        bot.send_message(message.from_user.id, text=texts_tree['hello'].format(message.from_user.username),
+                         reply_markup=inline_buttons)
         bot.register_next_step_handler(message, send_menu)
     delete_last_messages(message)
 
 
 @bot.message_handler(commands=['help'])
 def send_help(message):
-    bot.send_message(message.from_user.id, "Помогаю")
+    bot.send_message(message.from_user.id, texts_tree['help'])
     delete_last_messages(message.message)
 
 
 @bot.message_handler(commands=['menu'])  # реагирует на любую надпись в чате
 def send_menu(message):
     inline_buttons = get_inline_button(INLINE_MENU, 2)
-    question = 'Меню'
-    bot.send_message(message.from_user.id, text=question, reply_markup=inline_buttons)
-    delete_last_messages(message.message)
+    bot.send_message(message.from_user.id, text=texts_tree['menu'], reply_markup=inline_buttons)
+    delete_last_messages(message)
 
 
-@bot.message_handler(commands=['text'])  # реагирует на любую надпись в чате
+@bot.message_handler(content_types=['text'])  # реагирует на любую надпись в чате
 def wtf(message):
     inline_buttons = get_inline_button(INLINE_MENU, 2)
-    question = 'Не понимаю что ты говоришь'
-    bot.send_message(message.from_user.id, text=question, reply_markup=inline_buttons)
-    delete_last_messages(message.message)
+    bot.send_message(message.from_user.id, text=texts_tree['wtf'], reply_markup=inline_buttons)
+    delete_last_messages(message)
 
 
 @bot.callback_query_handler(lambda message: "menu" in message.data)
 def redirect_menu(message):
+    delete_last_messages(message.message)
     send_menu(message)
 
 
 @bot.callback_query_handler(lambda message: "theme" in message.data)
 def view_theme(message):
     theme_num = check_theme_num(message.data)
-    bot.send_message(message.message.chat.id, text=COURSES[theme_num],
-                     reply_markup=get_inline_button(edit_inline_button(theme_num, INLINE_VIEW_THEME)))
+    db.update_course_step(theme_num, message.from_user.id, True)
+    bot.send_message(message.from_user.id, text=COURSES[theme_num],
+                     reply_markup=get_inline_button(edit_inline_button(theme_num, INLINE_VIEW_THEME, db, message)))
     delete_last_messages(message.message)
 
 
@@ -64,23 +67,27 @@ def view_theme(message):
 def callback_worker(message):
     if "courses" in message.data:
         if "final" in message.data:
-            bot.send_message(message.message.chat.id, text="Вау ты прошёл весь курс",
-                             reply_markup=get_inline_button(INLINE_MENU))
+            if is_done_full_course(db, message):
+                bot.send_message(message.from_user.id, text=texts_tree['done_course'],
+                                 reply_markup=get_inline_button(INLINE_MENU))
+            else:
+                bot.send_message(message.from_user.id, text=texts_tree['not_done_course'],
+                                 reply_markup=get_inline_button(get_bool_theme(INLINE_THEMES, db, message)))
         else:
-            course_num = check_theme_num(message.data)
-            question = 'Выберите тему'
-            bot.send_message(message.message.chat.id, text=question, reply_markup=get_inline_button(INLINE_THEMES))
-    elif message.data == "test":
+            if not db.get_course_step(message.from_user.id):
+                db.insert_course_step(0, message.from_user.id, False)
+            bot.send_message(message.from_user.id, text=texts_tree['choose_themes'],
+                             reply_markup=get_inline_button(get_bool_theme(INLINE_THEMES, db, message)))
+    elif "test" in message.data:
         bot.answer_callback_query(callback_query_id=message.id,
                                   text="Внимание, для проходения теста вам потребует 10 минут свободного времени",
                                   show_alert=True)
-
-        inline_buttons = get_inline_button(INLINE_YES_NO, 2)
-        question = 'Вы уверены, что готовы пройти тест?'
-        bot.send_message(message.message.chat.id, text=question, reply_markup=inline_buttons)
+        bot.send_message(message.from_user.id, text=texts_tree["are_you_ready"],
+                         reply_markup=get_inline_button(INLINE_YES_NO, 2))
 
     elif message.data == "rating":
-        bot.send_message(message.message.chat.id, "А тут рейтинг")
+        bot.send_message(message.from_user.id, text=texts_tree["rating"])
+
     delete_last_messages(message.message)
     bot.answer_callback_query(callback_query_id=message.id)
 
