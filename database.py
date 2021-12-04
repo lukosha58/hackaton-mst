f7eac91cb7abb79f1581d50afb370d817d71b309
Vo5torg <ilya.mayorov.2003@list.ru>
2021-12-04 14:19:56 +0300

    Добавлены: функция создания кнопок, функция удаления последних сообщений

--- database.py
+++ database.py
@@ -4,57 +4,57 @@ conn = sqlite3.connect('database.db', check_same_thread=False)
 cursor = conn.cursor()
 
 
-def db_create_users_table():  # Создание таблицы пользователей
-    cursor.execute("""CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, username TEXT);""")
+def create_users_table():  # Создание таблицы пользователей
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY, username TEXT, fullname TEXT);""")
     conn.commit()
 
 
-def db_create_test_result_table():  # Создание таблицы результатов тестирований
+def create_test_result_table():  # Создание таблицы результатов тестирований
     cursor.execute("""CREATE TABLE IF NOT EXISTS test_result (user_id INT, time INT, result INT);""")
     conn.commit()
 
 
-def db_create_course_step_table():  # Создание таблицы, содержащей этап курса, на котором остановился пользователь
+def create_course_step_table():  # Создание таблицы, содержащей этап курса, на котором остановился пользователь
     cursor.execute("""CREATE TABLE IF NOT EXISTS course_step (user_id INT, step INT);""")
     conn.commit()
 
 
-def db_insert_user(user_id: int, username: str):  # Добавление пользователя
-    cursor.execute("""INSERT INTO users (user_id, username) VALUES (?, ?)""", (user_id, username))
+def insert_user(user_id: int, username: str, fullname: str):  # Добавление пользователя
+    cursor.execute("""INSERT INTO users (user_id, username, fullname) VALUES (?, ?, ?)""",
+                   (user_id, username, fullname))
     conn.commit()
 
 
-def db_insert_or_replace_result(user_id: int, time, result: int):  # Добавление или замена результата тестирования
-    cursor.execute("""REPLACE INTO test_result (user_id, time,result) VALUES (?, ?)""", (user_id, time, result))
+def insert_or_replace_result(user_id: int, time, result: int):  # Добавление или замена результата тестирования
+    cursor.execute("""REPLACE INTO test_result (user_id, time, result) VALUES (?, ?, ?)""", (user_id, time, result))
     conn.commit()
 
 
-def db_insert_or_replace_course_step(user_id: int, step: int):  # Добавление или замена этапа прохождения курса
+def insert_or_replace_course_step(user_id: int, step: int):  # Добавление или замена этапа прохождения курса
     cursor.execute("""INSERT INTO course_step (user_id, step) VALUES (?, ?)""", (user_id, step))
     conn.commit()
 
 
-def db_get_all_users():  # Получение всех пользователей
+def get_all_users():  # Получение всех пользователей
     cursor.execute("""SELECT * FROM users """)
     return cursor.fetchall()
 
 
-def db_get_user(user_id: int):  # Получение информации о пользователе
+def get_user(user_id: int):  # Получение информации о пользователе
     cursor.execute("""SELECT * FROM users WHERE user_id = (?)""", (user_id,))
     return cursor.fetchone()
 
 
-def db_get_result(user_id: int):  # Получение информации о результате прохождения теста
+def get_result(user_id: int):  # Получение информации о результате прохождения теста
     cursor.execute("""SELECT * FROM test_result WHERE user_id = (?)""", (user_id,))
     return cursor.fetchone()
 
 
-def db_get_all_results():  # Получение всех результатов прохождения теста
+def get_all_results():  # Получение всех результатов прохождения теста
     cursor.execute("""SELECT * FROM test_result """)
     return cursor.fetchall()
 
 
-def db_get_course_step(user_id: int):  # Получение информации о этапе курса
+def get_course_step(user_id: int):  # Получение информации о этапе курса
     cursor.execute("""SELECT * FROM course_step WHERE user_id = (?)""", (user_id,))
     return cursor.fetchone()
-
--- main.py
+++ main.py
@@ -1,64 +1,104 @@
 import telebot
-import database
+import database as db
 import sqlite3
 from telebot import types
 
 token = open("token").readline()
-
 bot = telebot.TeleBot(token)
-TO_CHAT_ID = 731253916
+
+db.create_users_table()
+db.create_test_result_table()
+db.create_course_step_table()
+
+INLINE_MENU = [(("Курсы", "courses"), ("Тесты", "test")), ("Рейтинг", "rating")]
+INLINE_THEMES = [("Тема 1", "theme_1"), ("Тема 2", "theme_2")]
+INLINE_YES_NO = [(("Да", "yes"), ("Нет", "no"))]
+BUTTON_MENU = ["Меню", ("пися", "попа")]
+
+
+def get_keyboard_button(button_items, resize_keyboard=True, one_time_keyboard=True):
+    keyboard = telebot.types.ReplyKeyboardMarkup(resize_keyboard=resize_keyboard, one_time_keyboard=one_time_keyboard)
+    for item in button_items:
+        if isinstance(item, tuple):
+            one_line_buttons = []
+            for i in item:
+                one_line_buttons.append(telebot.types.KeyboardButton(i))
+            keyboard.add(*one_line_buttons)
+        else:
+            keyboard.add(telebot.types.KeyboardButton(text=item))
+    return keyboard
+
+
+def get_inline_button(inline_items, row_width=3):
+    inline_buttons = telebot.types.InlineKeyboardMarkup(row_width=row_width)
+    for item in inline_items:
+        if isinstance(item[0], tuple):
+            one_line_buttons = []
+            for i in item:
+                one_line_buttons.append(telebot.types.InlineKeyboardButton(text=i[0], callback_data=i[1]))
+            inline_buttons.add(*one_line_buttons)
+        else:
+            inline_buttons.add(telebot.types.InlineKeyboardButton(text=item[0], callback_data=item[1]))
+    return inline_buttons
+
+
+def delete_last_messages(message):
+    bot.delete_message(message.chat.id, message.message_id)
+    try:
+        bot.delete_message(message.chat.id, message.message_id - 1)
+    except :
+        pass
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    database.db_create_users_table
     # проверка на зарег, если зарег выдать меню
-    bot.send_message(message.from_user.id, "Здраствуйте")
-    bot.send_message(message.from_user.id, "Пожалуйста введите ФИО")
-    # передача данных в бд
+    def get_username():  # получаем ФИО
+        db.insert_user(message.from_user.id, message.from_user.username, message.text)
+        bot.send_message(message.from_user.id, "Здраствуйте")
+
+    if not db.get_user(message.from_user.id):
+        bot.send_message(message.from_user.id, "Пожалуйста введите ФИО")
+        bot.register_next_step_handler(message, get_username)
+    else:
+        bot.send_message(message.from_user.id, "Здраствуйте")
+        bot.register_next_step_handler(message, send_menu)
+
+
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    bot.send_message(message.from_user.id, "Помогаю")
+    delete_last_messages(message)
 
 
 @bot.message_handler(content_types=['text'])  # реагирует на любую надпись в чате
 def send_menu(message):
-    keyboard = telebot.types.InlineKeyboardMarkup(row_width=2)  # наша клавиатура
-    key_Courses = telebot.types.InlineKeyboardButton(text='Курсы', callback_data='courses')  # кнопка «Да»
-    # keyboard.add(key_Courses)  # добавляем кнопку в клавиатуру
-    key_test = telebot.types.InlineKeyboardButton(text='Тесты', callback_data='test')
-    keyboard.add(key_Courses, key_test)
-    # start_markup.row(key_Courses, key_test)
-    key_rating = telebot.types.InlineKeyboardButton(text='Рейтинг', callback_data='rating')
-    keyboard.add(key_rating)
-    # start_markup.row(key_rating)
+    inline_buttons = get_inline_button(INLINE_MENU, 2)
     question = 'Меню'
-    bot.send_message(message.from_user.id, text=question, reply_markup=keyboard)
+    bot.send_message(message.from_user.id, text=question, reply_markup=inline_buttons)
+
+    delete_last_messages(message)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
     if call.data == "courses":
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        key_t1 = telebot.types.InlineKeyboardButton(text='Какая-то тема1', callback_data='t1')
-        keyboard.add(key_t1)
-        key_t2 = telebot.types.InlineKeyboardButton(text='Какая-то тема2', callback_data='t2')
-        keyboard.add(key_t2)
-        key_t3 = telebot.types.InlineKeyboardButton(text='Какая-то тема3', callback_data='t3')
-        keyboard.add(key_t3)
+        inline_buttons = get_inline_button(INLINE_THEMES)
         question = 'Выберите тему'
-        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=inline_buttons)
     elif call.data == "test":
         bot.answer_callback_query(callback_query_id=call.id,
                                   text="Внимание, для проходения теста вам потребует 10 минут свободного времени",
                                   show_alert=True)
-        keyboard = telebot.types.InlineKeyboardMarkup()
-        key_yes = telebot.types.InlineKeyboardButton(text='Да', callback_data='Yes')
-        keyboard.add(key_yes)
-        key_no = telebot.types.InlineKeyboardButton(text='Нет', callback_data='No')
-        keyboard.add(key_no)
+
+        inline_buttons = get_inline_button(INLINE_YES_NO, 2)
         question = 'Вы уверены, что готовы пройти тест?'
-        bot.send_message(call.message.chat.id, text=question, reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, text=question, reply_markup=inline_buttons)
 
     elif call.data == "rating":
         bot.send_message(call.message.chat.id, "А тут рейтинг")
+    delete_last_messages(call.message)
+    bot.answer_callback_query(callback_query_id=call.id)
 
 
 bot.infinity_polling()
