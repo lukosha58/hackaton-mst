c39b1d9aab452f822a3da6486f213244a25c3879
Vo5torg <ilya.mayorov.2003@list.ru>
2021-12-04 14:42:44 +0300

    Мяу

--- main.py
+++ main.py
@@ -46,23 +46,27 @@ def delete_last_messages(message):
     bot.delete_message(message.chat.id, message.message_id)
     try:
         bot.delete_message(message.chat.id, message.message_id - 1)
-    except :
+    except:
         pass
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     # проверка на зарег, если зарег выдать меню
-    def get_username():  # получаем ФИО
+    def get_username(message):  # получаем ФИО
+        inline_buttons = get_inline_button(INLINE_MENU, 2)
         db.insert_user(message.from_user.id, message.from_user.username, message.text)
-        bot.send_message(message.from_user.id, "Здраствуйте")
+        bot.send_message(message.from_user.id, text="Здраствуйте", reply_markup=inline_buttons)
+        delete_last_messages(message)
 
     if not db.get_user(message.from_user.id):
-        bot.send_message(message.from_user.id, "Пожалуйста введите ФИО")
+        bot.send_message(message.from_user.id, text="Пожалуйста введите ФИО")
         bot.register_next_step_handler(message, get_username)
     else:
-        bot.send_message(message.from_user.id, "Здраствуйте")
+        inline_buttons = get_inline_button(INLINE_MENU, 2)
+        bot.send_message(message.from_user.id, text="Здраствуйте", reply_markup=inline_buttons)
         bot.register_next_step_handler(message, send_menu)
+    delete_last_messages(message)
 
 
 @bot.message_handler(commands=['help'])
@@ -76,7 +80,6 @@ def send_menu(message):
     inline_buttons = get_inline_button(INLINE_MENU, 2)
     question = 'Меню'
     bot.send_message(message.from_user.id, text=question, reply_markup=inline_buttons)
-
     delete_last_messages(message)
 
 
