676e95197282e3060b0a723b373ed952ffcc9013
sanchess <Sanchess2015@yandex.ru>
2021-12-05 12:44:44 +0300

    Доделал qr

--- qrcode.py
+++ qrcode.py
@@ -8,9 +8,10 @@ def qr_code(message):
 	f_id = message.photo[len(message.photo)-1].file_id
 	file_name=bot.get_file(f_id)
 	down_file=bot.download_file(file_name.file_path)
-	with open(f'img_ {random.randint(1, 1000)}'+'.jpg', 'wb') as file:
+	number_qr=random.randint(1, 1000)
+	with open(f'img_{number_qr}'+'.jpg', 'wb') as file:
 		file.write(down_file)
-	img = cv2.imread("img_.jpg")
+	img = cv2.imread(f"img_{number_qr}.jpg")
 	barcodes = pyzbar.decode(img)
 
 	for barcode in barcodes:
