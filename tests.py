c29ad1b431d39f7535c5f702e8599e1e05dcf414
Alexey <lukosha58@yandex.ru>
2021-12-05 08:21:19 +0300

    add func check ans

--- tests.py
+++ tests.py
@@ -5,12 +5,26 @@ bot = telebot.TeleBot(token)
 
 
 def testing(message):
+    c = 1
+    c2 = 1
+    c3 = 2
     if message.data == "Test_places":
         file = open("test1", encoding="utf-8")
 
-        for i in file.readlines():
-            question, ans_1, ans_2 = i.split(";")
-            answers = [[ans_1, f"ans_1"], [ans_2, "ans_2"]]
+        for i in file.readlines()[::-1]:
+            question, ans_1, ans_2, right_answer = i.split(";")
+            answers = [[ans_1, ID_TESTS["pl"].format(c, c2)], [ans_2, ID_TESTS["pl"].format(c, c3)]]
             bot.send_message(message.message.chat.id, text=question, reply_markup=get_inline_button(answers, 2))
+            print(answers)
+
+            c += 1
+        file.close()
     elif message.data == "Test_qr":
         print("qr")
+    elif "pl" in message.data:
+        print(message.data)
+
+
+def check_ans(message):
+    if "pl" in message.data:
+        print(message.data)
